90f080f0b6999cc8bcea4183014b366f97314e01
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-02-14 19:14:00 +0100

    0.3.0

---
+++ src/components/pagebuttons.js
@@ -0,0 +1,12 @@
+import { Component } from 'preact';
+import style from './pagebuttons.less';
+
+export default class Pagebuttons extends Component {
+	render() {
+		return (
+			<div>
+
+			</div>
+		);
+	}
+}
