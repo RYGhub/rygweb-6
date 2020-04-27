198878b0cd0f7c55bbbeeea7ead6ef8492d10422
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-04-27 16:51:39 +0200

    Update pages

---
+++ docs/precache-manifest.9da67e0203b100da81ff367a0775691f.esm.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icon.png"
+  },
+  {
+    "revision": "a020bad388df3a144d03",
+    "url": "/bundle.b0683.esm.js"
+  },
+  {
+    "revision": "a020bad388df3a144d03",
+    "url": "/bundle.b9987.css"
+  },
+  {
+    "revision": "7e3f855b4c4b73d344c740c761889de7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a596390ebcee0e66821",
+    "url": "/polyfills.851f4.esm.js"
+  },
+  {
+    "revision": "f1c3cec47544463fccec",
+    "url": "/route-brawlhalladue.chunk.cb123.css"
+  },
+  {
+    "revision": "f1c3cec47544463fccec",
+    "url": "/route-brawlhalladue.chunk.ed6dd.esm.js"
+  },
+  {
+    "revision": "dcb802913fa2f41ce64b",
+    "url": "/route-diario.chunk.5536e.css"
+  },
+  {
+    "revision": "dcb802913fa2f41ce64b",
+    "url": "/route-diario.chunk.f7028.esm.js"
+  },
+  {
+    "revision": "7a7e506e32f322a8793c",
+    "url": "/route-homepage.chunk.c35a8.esm.js"
+  },
+  {
+    "revision": "7a7e506e32f322a8793c",
+    "url": "/route-homepage.chunk.e5226.css"
+  },
+  {
+    "revision": "74f60a424d84e7f78955",
+    "url": "/route-login.chunk.f1993.esm.js"
+  },
+  {
+    "revision": "81fdbf33359544e8e920",
+    "url": "/route-members.chunk.9e852.esm.js"
+  },
+  {
+    "revision": "81fdbf33359544e8e920",
+    "url": "/route-members.chunk.bd006.css"
+  },
+  {
+    "revision": "47adbc67b3601b9ee8c1",
+    "url": "/route-rygnavbar.chunk.a67ba.css"
+  },
+  {
+    "revision": "47adbc67b3601b9ee8c1",
+    "url": "/route-rygnavbar.chunk.bc846.esm.js"
+  },
+  {
+    "revision": "7e157ef3c42587530870",
+    "url": "/route-stats.chunk.dd489.esm.js"
+  },
+  {
+    "revision": "504fe6760bd23a08fea6",
+    "url": "/route-userprofile.chunk.d7bec.css"
+  },
+  {
+    "revision": "504fe6760bd23a08fea6",
+    "url": "/route-userprofile.chunk.db6bb.esm.js"
+  },
+  {
+    "revision": "15364632be53bc8af566",
+    "url": "/route-wikilist.chunk.a7769.css"
+  },
+  {
+    "revision": "15364632be53bc8af566",
+    "url": "/route-wikilist.chunk.ad5e8.esm.js"
+  }
+]);
