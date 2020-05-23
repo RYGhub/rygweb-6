42aca27827ce08d7e36d60517b4536821467c8a1
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-05-23 22:37:12 +0200

    ??????????????????????

---
+++ docs/precache-manifest.06d12a5a266877e82ccbcf77421aa2e8.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icon.png"
+  },
+  {
+    "revision": "e5e8394d4ee4e0a52ecf",
+    "url": "/bundle.c3c49.css"
+  },
+  {
+    "revision": "e5e8394d4ee4e0a52ecf",
+    "url": "/bundle.e5e83.js"
+  },
+  {
+    "revision": "2240aef24b69b106467a02f8062a6193",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a596390ebcee0e66821",
+    "url": "/polyfills.1a596.js"
+  },
+  {
+    "revision": "e6752717c40a9a21ce45",
+    "url": "/route-brawlhalladue.chunk.e6752.js"
+  },
+  {
+    "revision": "e6752717c40a9a21ce45",
+    "url": "/route-brawlhalladue.chunk.fcd8e.css"
+  },
+  {
+    "revision": "97f2119ad79187e7444f",
+    "url": "/route-diario.chunk.97f21.js"
+  },
+  {
+    "revision": "97f2119ad79187e7444f",
+    "url": "/route-diario.chunk.d3b22.css"
+  },
+  {
+    "revision": "57a1a942693dfcd95d0d",
+    "url": "/route-homepage.chunk.57a1a.js"
+  },
+  {
+    "revision": "57a1a942693dfcd95d0d",
+    "url": "/route-homepage.chunk.61fe1.css"
+  },
+  {
+    "revision": "74f60a424d84e7f78955",
+    "url": "/route-login.chunk.74f60.js"
+  },
+  {
+    "revision": "8d74892e02cf26980a1b",
+    "url": "/route-members.chunk.5c3e7.css"
+  },
+  {
+    "revision": "8d74892e02cf26980a1b",
+    "url": "/route-members.chunk.8d748.js"
+  },
+  {
+    "revision": "067d4ee19927e556ebe0",
+    "url": "/route-rygnavbar.chunk.03a16.css"
+  },
+  {
+    "revision": "067d4ee19927e556ebe0",
+    "url": "/route-rygnavbar.chunk.067d4.js"
+  },
+  {
+    "revision": "eac762e1a73a361154b7",
+    "url": "/route-stats.chunk.eac76.js"
+  },
+  {
+    "revision": "7d1a508f879159188d9a",
+    "url": "/route-userprofile.chunk.3559e.css"
+  },
+  {
+    "revision": "7d1a508f879159188d9a",
+    "url": "/route-userprofile.chunk.7d1a5.js"
+  },
+  {
+    "revision": "8ae5631719a75b63f1d6",
+    "url": "/route-wikilist.chunk.8ae56.js"
+  },
+  {
+    "revision": "8ae5631719a75b63f1d6",
+    "url": "/route-wikilist.chunk.de646.css"
+  },
+  {
+    "revision": "144c6ff1323d2d15182a7d2af08d027e",
+    "url": "/sw-debug.js"
+  },
+  {
+    "revision": "38e56aca87e283f1aba070e9cab09b95",
+    "url": "/sw-esm.js"
+  },
+  {
+    "revision": "bfc5bd18885255982238f3628770f48d",
+    "url": "/sw.js"
+  }
+]);
---
+++ docs/precache-manifest.1c167c03eba30bde7bb23313b6410ab2.esm.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icon.png"
+  },
+  {
+    "revision": "e5e8394d4ee4e0a52ecf",
+    "url": "/bundle.58189.esm.js"
+  },
+  {
+    "revision": "e5e8394d4ee4e0a52ecf",
+    "url": "/bundle.c3c49.css"
+  },
+  {
+    "revision": "2240aef24b69b106467a02f8062a6193",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a596390ebcee0e66821",
+    "url": "/polyfills.851f4.esm.js"
+  },
+  {
+    "revision": "e6752717c40a9a21ce45",
+    "url": "/route-brawlhalladue.chunk.8e4f0.esm.js"
+  },
+  {
+    "revision": "e6752717c40a9a21ce45",
+    "url": "/route-brawlhalladue.chunk.fcd8e.css"
+  },
+  {
+    "revision": "97f2119ad79187e7444f",
+    "url": "/route-diario.chunk.739ca.esm.js"
+  },
+  {
+    "revision": "97f2119ad79187e7444f",
+    "url": "/route-diario.chunk.d3b22.css"
+  },
+  {
+    "revision": "57a1a942693dfcd95d0d",
+    "url": "/route-homepage.chunk.61fe1.css"
+  },
+  {
+    "revision": "57a1a942693dfcd95d0d",
+    "url": "/route-homepage.chunk.81c1d.esm.js"
+  },
+  {
+    "revision": "74f60a424d84e7f78955",
+    "url": "/route-login.chunk.f1993.esm.js"
+  },
+  {
+    "revision": "8d74892e02cf26980a1b",
+    "url": "/route-members.chunk.5c3e7.css"
+  },
+  {
+    "revision": "8d74892e02cf26980a1b",
+    "url": "/route-members.chunk.ad69e.esm.js"
+  },
+  {
+    "revision": "067d4ee19927e556ebe0",
+    "url": "/route-rygnavbar.chunk.03a16.css"
+  },
+  {
+    "revision": "067d4ee19927e556ebe0",
+    "url": "/route-rygnavbar.chunk.cfe74.esm.js"
+  },
+  {
+    "revision": "eac762e1a73a361154b7",
+    "url": "/route-stats.chunk.bac6d.esm.js"
+  },
+  {
+    "revision": "7d1a508f879159188d9a",
+    "url": "/route-userprofile.chunk.3559e.css"
+  },
+  {
+    "revision": "7d1a508f879159188d9a",
+    "url": "/route-userprofile.chunk.e91ed.esm.js"
+  },
+  {
+    "revision": "8ae5631719a75b63f1d6",
+    "url": "/route-wikilist.chunk.aedc5.esm.js"
+  },
+  {
+    "revision": "8ae5631719a75b63f1d6",
+    "url": "/route-wikilist.chunk.de646.css"
+  }
+]);
