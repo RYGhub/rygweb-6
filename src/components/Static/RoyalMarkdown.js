5125bf9391efefed98be4ed0157926e87994645e
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-06-28 18:06:20 +0200

    0.6.6

--- src/components/Static/RoyalMarkdown.js
+++ src/components/Static/RoyalMarkdown.js
@@ -1,10 +1,16 @@
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown from 'react-markdown/with-html';
 import AnyLink from '../Dynamic/Link';
+import { Image } from 'bluelib';
 
 export default function (props) {
 	return (
-		<ReactMarkdown source={props.children} renderers={{
-			"link": AnyLink
-		}}/>
+		<ReactMarkdown
+			source={props.children}
+			renderers={{
+				"link": AnyLink,
+				"image": Image
+			}}
+			escapeHtml={false}
+		/>
 	);
 }
