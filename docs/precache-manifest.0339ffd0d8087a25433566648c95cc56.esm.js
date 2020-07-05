993bf2b35ae09637e5f1989dbc89e89e8aff85e2
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-07-05 11:30:23 +0200

    0.7.7

---
+++ docs/precache-manifest.0339ffd0d8087a25433566648c95cc56.esm.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icon.png"
+  },
+  {
+    "revision": "b736387aa3041e889af2",
+    "url": "/bundle.42739.css"
+  },
+  {
+    "revision": "b736387aa3041e889af2",
+    "url": "/bundle.48977.esm.js"
+  },
+  {
+    "revision": "e5b2e3ab4588d77018b49a0ebbb6cb4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fef7d7e419affe3c2fe8",
+    "url": "/polyfills.2f29a.esm.js"
+  },
+  {
+    "revision": "40128937652638a959f8",
+    "url": "/route-Home.chunk.4d84d.css"
+  },
+  {
+    "revision": "40128937652638a959f8",
+    "url": "/route-Home.chunk.c7027.esm.js"
+  },
+  {
+    "revision": "8e8700c23feceb677e8c",
+    "url": "/route-MembersList.chunk.0befe.css"
+  },
+  {
+    "revision": "8e8700c23feceb677e8c",
+    "url": "/route-MembersList.chunk.b02f3.esm.js"
+  },
+  {
+    "revision": "8ed8c1a2e02a018d1de7",
+    "url": "/route-Profile.chunk.118dd.css"
+  },
+  {
+    "revision": "8ed8c1a2e02a018d1de7",
+    "url": "/route-Profile.chunk.e4398.esm.js"
+  },
+  {
+    "revision": "454ff030a4a0620118d0",
+    "url": "/route-WikiList.chunk.30935.esm.js"
+  }
+]);
