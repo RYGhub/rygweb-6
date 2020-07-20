152754ab5669bf180f1d07e811a551beed3345ba
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-07-20 22:31:54 +0200

    Sposta i titoli della wiki a sinistra

--- src/components/Static/RoyalMarkdown.js
+++ src/components/Static/RoyalMarkdown.js
@@ -1,16 +1,19 @@
 import ReactMarkdown from 'react-markdown';
 import AnyLink from '../Dynamic/Link';
 import { Image } from 'bluelib';
+import style from './RoyalMarkdown.less';
 
 export default function (props) {
 	return (
-		<ReactMarkdown
-			source={props.children}
-			renderers={{
-				"link": AnyLink,
-				"image": Image
-			}}
-			escapeHtml={false}
-		/>
+		<div class={style.royalmarkdown}>
+			<ReactMarkdown
+				source={props.children}
+				renderers={{
+					"link": AnyLink,
+					"image": Image
+				}}
+				escapeHtml={false}
+			/>
+		</div>
 	);
 }
